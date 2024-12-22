b34e6af6d4567e61ff2a7c98551516a9c0e361b5
Matthias Bruns <matthiasbruns35@gmail.com>
2024-12-22 19:06:26 +0100

    add initial window with logger

---
+++ include/utils/log.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/stdout_sinks.h"
+
+#define CG_ASSERTIONS
+
+#define CG_THROW(...)                   \
+{                                       \
+  spdlog::critical(__VA_ARGS__);        \
+  std::string s(__FILE__);              \
+  s.append(": ");                       \
+  s.append(std::to_string(__LINE__));   \
+  spdlog::throw_spdlog_ex(s);           \
+}
+
+#if defined(CG_ASSERTIONS)
+#define CG_ASSERT(X, ...) if (!(X)) CG_THROW(__VA_ARGS__);
+#else
+#define CG_ASSERT(X, ...) X
+#endif
+
+namespace cglog = spdlog;
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include "utils/log.hpp"
+#include <QApplication>
+#include <QMainWindow>
+
+int main(int argc, char** argv)
+{
+  std::vector<spdlog::sink_ptr> sinks;
+  sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>());
+  sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_st>("CashGuard.log", true));
+  auto combined_logger = std::make_shared<spdlog::logger>("default_logger", sinks.begin(), sinks.end());
+  spdlog::set_default_logger(combined_logger);
+  spdlog::set_level(spdlog::level::debug);
+  spdlog::set_pattern("[%Y-%m-%d %T.%e] [%L] %v");
+
+	QApplication app(argc, argv);
+
+	QMainWindow mainWindow;
+	mainWindow.setWindowTitle("CashGuard");
+	mainWindow.resize(800, 600);
+	mainWindow.show();
+
+	return app.exec();
+}
