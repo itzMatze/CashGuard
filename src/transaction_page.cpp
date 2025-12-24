74b759a147e84ab4e93b716d945c7da7e97925da
Matthias Bruns <matthiasbruns35@gmail.com>
2025-12-24 13:50:15 +0100

    extract some code into functions

--- src/transaction_page.cpp
+++ src/transaction_page.cpp
@@ -19,6 +19,8 @@ void TransactionPage::draw(ImVec2 available_space, TransactionModel& transaction
 	ImGui::PushStyleColor(ImGuiCol_Button, button_color);
 	ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(button_color.x + 0.1f, button_color.y + 0.1f, button_color.z + 0.1f, button_color.w));
 	ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(button_color.x - 0.1f, button_color.y - 0.1f, button_color.z - 0.1f, button_color.w));
+
+	// Add
 	if (ImGui::Button("Add", button_size))
 	{
 		opened_transaction = Transaction();
@@ -28,6 +30,8 @@ void TransactionPage::draw(ImVec2 available_space, TransactionModel& transaction
 	ImGui::SameLine();
 	ImGui::Button("Add Group", button_size);
 	ImGui::SameLine();
+
+	// Edit
 	if (ImGui::Button("Edit", button_size) && row_valid)
 	{
 		opened_transaction = *transaction_model.at(row_index);
@@ -38,6 +42,8 @@ void TransactionPage::draw(ImVec2 available_space, TransactionModel& transaction
 		transaction_dialog(transaction_model, row_index);
 	}
 	ImGui::SameLine();
+
+	// Remove
 	if (ImGui::Button("Remove", button_size) && row_valid) ImGui::OpenPopup("Transaction Remove##Dialog");
 	if (ImGui::BeginPopupModal("Transaction Remove##Dialog", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
 	{
@@ -53,6 +59,8 @@ void TransactionPage::draw(ImVec2 available_space, TransactionModel& transaction
 	}
 	ImGui::PopStyleColor(3);
 	ImGui::SameLine();
+
+	// Accounts
 	ImVec4 account_button_color(0.0f, 0.7f, 0.0f, 1.0f);
 	if (account_model.get_total_amount().value != transaction_model.get_global_total_amount().value) account_button_color = ImVec4(0.7f, 0.0f, 0.0f, 1.0f);
 	ImGui::PushStyleColor(ImGuiCol_Button, account_button_color);
@@ -62,11 +70,11 @@ void TransactionPage::draw(ImVec2 available_space, TransactionModel& transaction
 	ImGui::PopStyleColor(3);
 }
 
-void TransactionPage::transaction_dialog(TransactionModel& transaction_model, int32_t transaction_index)
+Date date_input(const Date& date)
 {
-	int32_t day = uint32_t(opened_transaction.date.day());
-	int32_t month = uint32_t(opened_transaction.date.month());
-	int32_t year = int32_t(opened_transaction.date.year());
+	int32_t day = uint32_t(date.day());
+	int32_t month = uint32_t(date.month());
+	int32_t year = int32_t(date.year());
 	ImGui::PushItemWidth(50.0f);
 	ImGui::InputInt("##Day", &day, 0, 0);
 	ImGui::SameLine();
@@ -82,31 +90,42 @@ void TransactionPage::transaction_dialog(TransactionModel& transaction_model, in
 	ImGui::PopItemWidth();
 	// clamp date
 	month = std::clamp(month, 1, 12);
-	opened_transaction.date = to_date(day, month, year);
-	std::chrono::year_month_day_last day_last(opened_transaction.date.year(), std::chrono::month_day_last(opened_transaction.date.month()));
+	Date new_date = to_date(day, month, year);
+	std::chrono::year_month_day_last day_last(new_date.year(), std::chrono::month_day_last(new_date.month()));
 	day = std::clamp(day, 1, int32_t(uint32_t(day_last.day())));
-	opened_transaction.date = to_date(day, month, year);
-	std::array<char, 1024> description_input;
-	std::snprintf(description_input.data(), description_input.size(), "%s", opened_transaction.description.c_str());
-	ImGui::InputTextWithHint("##Description", "Description", description_input.data(), 1024);
-	opened_transaction.description = std::string(description_input.data(), strnlen(description_input.data(), description_input.size()));
-	std::array<char, 1024> amount_input;
-	std::snprintf(amount_input.data(), amount_input.size(), "%s", opened_transaction.amount.to_string().c_str());
-	ImGui::InputTextWithHint("##Amount", "Amount", amount_input.data(), 1024);
-	bool parse_success = to_amount(std::string(amount_input.data(), strnlen(amount_input.data(), amount_input.size())), opened_transaction.amount);
-	// category dropdown
-	if (ImGui::BeginCombo("##Category", opened_transaction.category.c_str()))
+	return to_date(day, month, year);
+}
+
+std::string string_input(const char* label, const char* hint, const std::string& text)
+{
+	std::array<char, 1024> input;
+	std::snprintf(input.data(), input.size(), "%s", text.c_str());
+	ImGui::InputTextWithHint(label, hint, input.data(), 1024);
+	return std::string(input.data(), strnlen(input.data(), input.size()));
+}
+
+std::string category_dropdown(const std::string& category, const std::vector<std::string>& category_names)
+{
+	std::string new_category = category;
+	if (ImGui::BeginCombo("##Category", category.c_str()))
 	{
-		const std::vector<std::string>& category_names = transaction_model.get_category_names();
 		for (int i = 0; i < category_names.size(); ++i)
 		{
-			bool selected = (category_names[i] == opened_transaction.category);
-			if (ImGui::Selectable(category_names[i].c_str(), selected)) opened_transaction.category = category_names[i];
+			bool selected = (category_names[i] == category);
+			if (ImGui::Selectable(category_names[i].c_str(), selected)) new_category = category_names[i];
 			if (selected) ImGui::SetItemDefaultFocus();
 		}
 		ImGui::EndCombo();
 	}
+	return new_category;
+}
 
+void TransactionPage::transaction_dialog(TransactionModel& transaction_model, int32_t transaction_index)
+{
+	opened_transaction.date = date_input(opened_transaction.date);
+	opened_transaction.description = string_input("##Description", "Description", opened_transaction.description);
+	bool parse_success = to_amount(string_input("##Amount", "Amount", opened_transaction.get_field_view(TransactionFieldNames::Amount)), opened_transaction.amount);
+	opened_transaction.category = category_dropdown(opened_transaction.category, transaction_model.get_category_names());
 	if (ImGui::Button("OK"))
 	{
 		if (transaction_index == -1)
